d45e111a8369c368bf111c551466bca59c06028d
SamMathaniha <mathanihasam@gmail.com>
2024-08-12 13:04:07 +0530

    Added the index page

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\ProfileController;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class, 'home']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
